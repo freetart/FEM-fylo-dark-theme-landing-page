ef798515d844a1fa9c10f6ce96b23a70a6f97e46
habib <habibdev96@gmail.com>
2021-06-11 14:38:01 -0600

    added aos

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
+import AOS from "aos";
 import preloader from "./preloader.js";
 import validateEmail from "./validateEmail.js";
 
 const init = () => {
   preloader();
   validateEmail();
+  AOS.init({ offset: 100, duration: 1000, once: true });
 };
 
 window.addEventListener("DOMContentLoaded", init);
